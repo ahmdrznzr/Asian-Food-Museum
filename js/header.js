675d408918a8b24a970250bd5ae369fde003e6cb
Zahra Faraji <zahrafaraji467@gmail.com>
2025-01-20 13:23:41 +0100

    change the style
    add up button on narrative page. also change the style of the buttons.

--- js/header.js
+++ js/header.js
@@ -4,3 +4,24 @@ const collapse = document.querySelector('.collapse');
 toggler.addEventListener('click', () => {
     collapse.classList.toggle('show');
 });
+
+// up button javascript code to work
+
+const scrollToTopButton = document.getElementById("scrollToTop");
+
+// Show button on scroll
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) { // Show button after scrolling 300px
+        scrollToTopButton.classList.add("show-scroll-button");
+    } else {
+        scrollToTopButton.classList.remove("show-scroll-button");
+    }
+});
+
+// Scroll to top on button click
+scrollToTopButton.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
