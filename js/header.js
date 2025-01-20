0f3d4c021da608639377133f908b7bd81c91809f
Zahra Faraji <zahrafaraji467@gmail.com>
2025-01-20 13:28:41 +0100

    add comment

--- js/header.js
+++ js/header.js
@@ -1,9 +1,11 @@
+// collapse button on responsive menu
 const toggler = document.querySelector('.navbar-toggler');
 const collapse = document.querySelector('.collapse');
 
 toggler.addEventListener('click', () => {
     collapse.classList.toggle('show');
 });
+
 
 // up button javascript code to work
 
