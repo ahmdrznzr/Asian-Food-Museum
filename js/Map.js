fd228ea11e0a9957c1defa0f0e41c058aa4330b3
Nazanin.FKH <nazanin.fakharian@gmail.com>
2025-02-14 12:22:24 +0100

    Images in Map folder renamed.  Image paths in map.js modified

--- js/Map.js
+++ js/Map.js
@@ -21,91 +21,91 @@ document.addEventListener("DOMContentLoaded", () => {
         time: {
             ancient: {
                 items: ["Chinese Hot Pot (火锅)", "Lebanese Kibbeh (كبة)", "Lebanese Filo Pastry (كنافة)", "Persian Noodle Soup (آش رشته)", "Persian Herb Stew (قرمه سبزی)", "Chinese Sour Soup with Beef Slices (酸汤肥牛)"],
-                floorPlan: "images/Maps/Slide13.PNG",
-                map: "images/Maps/Slide14.PNG",
+                floorPlan: "images/Maps/13_floorplan_ancient.PNG",
+                map: "images/Maps/12_map_ancient.PNG",
             },
             medieval: {
                 items: ["Lebanese Hummus (حمص)", "Chinese Peking Duck (北京烤鸭)", "Persian Barberry and Saffron Rice Cake with Chicken (تهچین مرغ)", "Persian Saffron Ice cream (بستنی سنتی)", "Pakistani Deep-fried Lentil Fritters (دہی بھلے چاٹ)", "Pakistani Chicken Pilaf (چکن بریانی", "Pakistani Mutton in Clay Pot (مٹن کُنا)", "Pakistani Semolina Halwa (سوجی کا حلوہ)", "Japanese Tempura (天婦羅)"],
-                floorPlan: "floor-medieval.png",
-                map: "map-medieval.png",
+                floorPlan: "images/Maps/16_floorplan_medieval.png",
+                map: "images/Maps/15_map_medieval.png",
             },
             modern: {
                 items: ["Japanese Sukiyaki (すき焼き)", "Japanese Pork Bone Ramen (豚骨ラーメン)", "Japanese Rotation Sushi (回転寿司)", "Lebanese Meat wrap (شاورما)", "Chinese Coca-Cola Chicken Wings (可乐鸡翅)"],
-                floorPlan: "floor-modern.png",
-                map: "map-modern.png",
+                floorPlan: "images/Maps/19_floorplan_modern.png",
+                map: "images/Maps/18_map_modern.png",
             },
         },
         ingredients: {
             proteinBased: {
                 items: ["Chinese Hot Pot (火锅)", "Lebanese Kibbeh (كبة)", "Persian Herb Stew (قرمه سبزی)", "Chinese Sour Soup with Beef Slices (酸汤肥牛)", "Chinese Peking Duck (北京烤鸭)", "Persian Barberry and Saffron Rice Cake with Chicken (تهچین مرغ)", "Pakistani Chicken Pilaf (چکن بریانی)", "Pakistani Mutton in Clay Pot (مٹن کُنا)", "Japanese Tempura (天婦羅)", "Japanese Sukiyaki (すき焼き)", "Japanese Pork Bone Ramen (豚骨ラーメン)", "Japanese Rotation Sushi (回転寿司)", "Lebanese Meat wrap (شاورما)", "Chinese Coca-Cola Chicken Wings (可乐鸡翅)"],
-                floorPlan: "floorplan_proteinbased.png",
-                map: "map_proteinbased.png",
+                floorPlan: "images/Maps/23_floorplan_proteinbased.png",
+                map: "images/Maps/22_map_proteinbased.png",
             },
             plantBased: {
                 items: ["Chinese Hot Pot (火锅)", "Persian Noodle Soup (آش رشته)", "Lebanese Hummus (حمص)", "Pakistani Deep-fried Lentil Fritters (دہی بھلے چاٹ)", "Japanese Tempura (天婦羅)", "Japanese Rotation Sushi (回転寿司)"],
-                floorPlan: "floorplan_plantbased.png",
-                map: "map_plantbased.png",
+                floorPlan: "images/Maps/26_floorplan_plantbased.png",
+                map: "images/Maps/25_map_plantbased.png",
             },
             dairyBased: {
                 items: ["Lebanese Filo Pastry (كنافة)", "Persian Saffron Ice cream (بستنی سنتی)", "Pakistani Semolina Halwa (سوجی کا حلوہ)"],
-                floorPlan: "floorplan_dairybased.png",
-                map: "map_dairybased.png",
+                floorPlan: "images/Maps/29_floorplan_dairybased.png",
+                map: "images/Maps/28_map_dairybased.png",
             },
         },
         meal: {
             appetizer: {
                 items: ["Lebanese Kibbeh (كبة)", "Lebanese Filo Pastry (كنافة)", "Pakistani Deep-fried Lentil Fritters (دہی بھلے چاٹ)", "Japanese Tempura (天婦羅)", "Japanese Rotation Sushi (回転寿司)", "Chinese Coca-Cola Chicken Wings (可乐鸡翅)"],
-                floorPlan: "floorplan_appetizer.png",
-                map: "map_appetizer.png",
+                floorPlan: "images/Maps/43_floorplan_appetizer.png",
+                map: "images/Maps/42_map_appetizer.png",
             },
             mainCourse: {
                 items: ["Chinese Hot Pot (火锅)", "Lebanese Kibbeh (كبة)", "Lebanese Filo Pastry (كنافة)", "Persian Noodle Soup (آش رشته)", "Persian Herb Stew (قرمه سبزی)", "Chinese Sour Soup with Beef Slices (酸汤肥牛)", "Chinese Peking Duck (北京烤鸭)", "Persian Barberry and Saffron Rice Cake with Chicken (تهچین مرغ)", "Pakistani Deep-fried Lentil Fritters (دہی بھلے چاٹ)", "Pakistani Chicken Pilaf (چکن بریانی", "Pakistani Mutton in Clay Pot (مٹن کُنا)", "Japanese Tempura (天婦羅)", "Japanese Sukiyaki (すき焼き)", "Japanese Pork Bone Ramen (豚骨ラーメン)", "Japanese Rotation Sushi (回転寿司)", "Lebanese Meat wrap (شاورما)"],
-                floorPlan: "floorplan_maincourse.png",
-                map: "map_maincourse.png",
+                floorPlan: "images/Maps/46_floorplan_maincourse.png",
+                map: "images/Maps/45_map_maincourse.png",
             },
             dessert: {
                 items: ["Lebanese Filo Pastry (كنافة)", "Persian Saffron Ice cream (بستنی سنتی)", "Pakistani Semolina Halwa (سوجی کا حلوہ)"],
-                floorPlan: "floorplan_dessert.png",
-                map: "map_dessert.png",
+                floorPlan: "images/Maps/49_floorplan_dessert.png",
+                map: "images/Maps/48_map_dessert.png",
             },
         },
         preparation: {
             grilled: {
                 items: ["Lebanese Filo Pastry (كنافة)", "Chinese Peking Duck (北京烤鸭)", "Persian Barberry and Saffron Rice Cake with Chicken (تهچین مرغ)", "Lebanese Meat wrap (شاورما)", "Chinese Coca-Cola Chicken Wings (可乐鸡翅)"],
-                floorPlan: "floorplan_grilled.png",
-                map: "map_grilled.png",
+                floorPlan: "images/Maps/53_floorplan_grilled.png",
+                map: "images/Maps/52_map_grilled.png",
             },
             fried: {
                 items: ["Lebanese Kibbeh (كبة)", "Pakistani Deep-fried Lentil Fritters (دہی بھلے چاٹ)", "Japanese Tempura (天婦羅)"],
-                floorPlan: "floorplan_fried.png",
-                map: "map_fried.png",
+                floorPlan: "images/Maps/56_floorplan_fried.png",
+                map: "images/Maps/55_map_fried.png",
             },
             stewed: {
                 items: ["Chinese Hot Pot (火锅)", "Persian Noodle Soup (آش رشته)", "Persian Herb Stew (قرمه سبزی)", "Chinese Sour Soup with Beef Slices (酸汤肥牛)", "Pakistani Chicken Pilaf (چکن بریانی", "Pakistani Mutton in Clay Pot (مٹن کُنا)", "Pakistani Semolina Halwa (سوجی کا حلوہ)", "Japanese Sukiyaki (すき焼き)", "Japanese Pork Bone Ramen (豚骨ラーメン)"],
-                floorPlan: "floorplan_stewed.png",
-                map: "map_stewed.png",
+                floorPlan: "images/Maps/59_floorplan_stewed.png",
+                map: "images/Maps/58_map_stewed.png",
             },
             noCook: {
                 items: ["Lebanese Hummus (حمص)", "Persian Saffron Ice cream (بستنی سنتی)", "Japanese Rotation Sushi (回転寿司)"],
-                floorPlan: "floorplan_nocook.png",
-                map: "map_nocook.png",
+                floorPlan: "images/Maps/62_floorplan_nocook.png",
+                map: "images/Maps/61_map_nocook.png",
             },
         },
         region: {
             eastAsia: {
                 items: ["Chinese Hot Pot (火锅)", "Chinese Sour Soup with Beef Slices (酸汤肥牛)", "Chinese Peking Duck (北京烤鸭", "Japanese Tempura (天婦羅)", "Japanese Sukiyaki (すき焼き)", "Japanese Pork Bone Ramen (豚骨ラーメン)", "Japanese Rotation Sushi (回転寿司)", "Chinese Coca-Cola Chicken Wings (可乐鸡翅)"],
-                floorPlan: "floorplan_eastasia.png",
-                map: "map_eastasia.png",
+                floorPlan: "images/Maps/33_floorplan_eastasia.png",
+                map: "images/Maps/32_map_eastasia.png",
             },
             southAsia: {
                 items: ["Pakistani Deep-fried Lentil Fritters (دہی بھلے چاٹ)", "Pakistani Chicken Pilaf (چکن بریانی", "Pakistani Mutton in Clay Pot (مٹن کُنا)", "Pakistani Semolina Halwa (سوجی کا حلوہ)"],
-                floorPlan: "floorplan_southasia.png",
-                map: "map_southasia.png",
+                floorPlan: "images/Maps/36_floorplan_southasia.png",
+                map: "images/Maps/35_map_southasia.png",
             },
             westAsia: {
                 items: ["Lebanese Kibbeh (كبة)", "Lebanese Filo Pastry (كنافة)", "Persian Noodle Soup (آش رشته)", "Persian Herb Stew (قرمه سبزی)", "Persian Barberry and Saffron Rice Cake with Chicken (تهچین مرغ)", "Persian Saffron Ice cream (بستنی سنتی)", "Lebanese Meat wrap (شاورما)"],
-                floorPlan: "floorplan_westasia.png",
-                map: "map_westasia.png",
+                floorPlan: "images/Maps/39_floorplan_westasia.png",
+                map: "images/Maps/38_map_westasia.png",
             },
         }
     };
