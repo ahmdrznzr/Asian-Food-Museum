134ed2aeb1184a2431012a51cd7a7c6ef9161ae0
Zahra Faraji <79403254+zahrafarajih@users.noreply.github.com>
2025-02-19 16:02:38 +0100

    finalize index page

--- js/header.js
+++ js/header.js
@@ -10,12 +10,3 @@ toggler.addEventListener('click', () => {
 // up button javascript code to work
 
 const scrollToTopButton = document.getElementById("scrollToTop");
-
-// Show button on scroll
-window.addEventListener("scroll", () => {
-    if (window.scrollY > 300) { // Show button after scrolling 300px
-        scrollToTopButton.classList.add("show-scroll-button");
-    } else {
-        scrollToTopButton.classList.remove("show-scroll-button");
-    }
-});
--- js/scroll.js
+++ js/scroll.js
@@ -1,3 +1,12 @@
+// Show button on scroll
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) { // Show button after scrolling 300px
+        scrollToTopButton.classList.add("show-scroll-button");
+    } else {
+        scrollToTopButton.classList.remove("show-scroll-button");
+    }
+});
+
 // Scroll to top on button click
 scrollToTopButton.addEventListener("click", () => {
     window.scrollTo({
