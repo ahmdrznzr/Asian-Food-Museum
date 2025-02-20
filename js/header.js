2637372dd13bdb9be13cf3b7970978ff17a05b06
Zahra Faraji <79403254+zahrafarajih@users.noreply.github.com>
2025-02-20 09:57:37 +0100

    updating styles

--- js/header.js
+++ js/header.js
@@ -5,3 +5,18 @@ const collapse = document.querySelector('.collapse');
 toggler.addEventListener('click', () => {
     collapse.classList.toggle('show');
 });
+
+//change the color of active buttons on menu
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", function() {
+            // Remove 'active-btn' class from all nav links
+            navLinks.forEach(item => item.classList.remove("active-btn"));
+
+            // Add 'active-btn' class to the clicked nav link
+            this.classList.add("active-btn");
+        });
+    });
+});
