bb2be4b21f53af49814d03ba410563cee82f1c56
Ahmadreza <132892495+ahmdrznzr@users.noreply.github.com>
2025-02-20 13:59:49 +0100

    Update items.js
    back

--- js/items.js
+++ js/items.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", async function() {
     // Fetch JSON data - Ahamdreza
     async function fetchItems() {
         try {
-            const response = await fetch("../data/items.js");
+            const response = await fetch("data/items.json");
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
